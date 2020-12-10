683dfdeb3b68724591673676a148de0c3a0000dc
zjw <1245940381@qq.com>
2020-12-10 17:12:54 +0800

    第二次

--- app/src/main/java/com/example/svn/MainActivity.java
+++ app/src/main/java/com/example/svn/MainActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 public class MainActivity extends AppCompatActivity {
 
     String a="123";
+    String b="456";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
