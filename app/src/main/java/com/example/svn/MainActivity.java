bfa5eaa88c872a747df6fe503c07597a427c62c5
zjw <1245940381@qq.com>
2020-12-10 17:06:22 +0800

    第一次

---
+++ app/src/main/java/com/example/svn/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.svn;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    String a="123";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
